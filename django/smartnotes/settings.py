ae05bfe762be7f1e9fe80d78aa6dfd461ae04679
PrashantSBasnet <prasbas97@gmail.com>
2025-11-25 10:52:10 +0545

    feat: create app and modify view to return http response

--- django/smartnotes/settings.py
+++ django/smartnotes/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # todo: apps we created must be added here
+    'home',
 ]
 
 MIDDLEWARE = [
