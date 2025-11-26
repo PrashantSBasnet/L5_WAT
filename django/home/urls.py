8c5554445c6ea7799239b0044af961285a31b9fd
PrashantSBasnet <prasbas97@gmail.com>
2025-11-26 17:29:35 +0545

    feat: class based views

--- django/home/urls.py
+++ django/home/urls.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
-from .views import authorized
 
 urlpatterns=[
     path('admin/', admin.site.urls),
-    path('home/', views.home),
-    path('authorized/', views.authorized),
+    path('home/', views.HomeView.as_view()),
+    path('authorized/', views.AuthorizedView.as_view()),
 ]
--- django/notes/views.py
+++ django/notes/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes_list.html"
 
 
-def detail(request, pk):
-    try:
-        note_details = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404('Note does not exist')
-    return render(request, 'notes_list.html', {'note': note_details})
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes_detail.html"
