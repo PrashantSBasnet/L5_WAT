6a76b2b529244e57345b44b3fb893e4b6ce41ade
Prashant <prasbas97@gmail.com>
2025-11-30 20:27:26 +0545

    fix: signup page

--- django/home/views.py
+++ django/home/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 
 '''Class-Based Views'''
 
@@ -32,5 +32,17 @@ class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = "home/authorized.html"
     login_url = "/admin"
 
-class SignUpView(UserCreationForm):
+class SignUpView(CreateView):
+    form_class=UserCreationForm
     template_name = "home/signup.html"
+    success_url="/login"
+
+
+    #we are overriding the default validation for testing purpose
+    def get_form(self,form_class=None):
+        form=super().get_form(form_class)
+        form.fields["username"].help_text=None
+        form.fields["password1"].help_text=None
+        form.fields["password2"].help_text=None
+        return form
+
