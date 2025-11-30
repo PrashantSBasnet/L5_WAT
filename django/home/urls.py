b4f7faab19d949b8fd3678691f4dedc8a01ff297
Prashant <prasbas97@gmail.com>
2025-11-30 18:07:14 +0545

    fix: logout in the navbar

--- django/home/urls.py
+++ django/home/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.HomeView.as_view()),
-    path('authorized', views.AuthorizedView.as_view()),
+    path('', views.HomeView.as_view(), name='home'),
+    path('authorized', views.AuthorizedView.as_view(), name='authorized'),
     path('login', views.LoginInterfaceView.as_view(), name='login'),
     path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
 ]
